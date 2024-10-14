6b1c113f41af4f3ae969bf7d219a6d774890d21b
Llewellyn Pritchard <xacc.ide@gmail.com>
2024-10-14 15:44:48 +0200

    Allow EmbedResources to add netmodules

--- IronScheme.Build/EmbedResources.cs
+++ IronScheme.Build/EmbedResources.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Build.Framework;
+﻿using System.Resources;
+using Microsoft.Build.Framework;
 using Mono.Cecil;
 using Task = Microsoft.Build.Utilities.Task;
 
@@ -11,6 +12,8 @@ namespace IronScheme.Build
 
         [Required]
         public ITaskItem[] Resources { get; set; } = [];
+
+        public bool AsNetModule { get; set; }
 
         [Output]
         public ITaskItem Output { get; set; }
@@ -27,16 +30,46 @@ namespace IronScheme.Build
 
             var hasPdb = File.Exists(Path.ChangeExtension(input, "pdb"));
             using var ass = AssemblyDefinition.ReadAssembly(input, new ReaderParameters { ThrowIfSymbolsAreNotMatching = true, ReadSymbols = hasPdb, InMemory = input == output });
-            var resources = ass.MainModule.Resources;
 
-            foreach (var embed in Resources)
+            if (AsNetModule)
             {
-                var file = embed.ItemSpec;
-                var mname = embed.GetMetadata("Name");
-                var name = string.IsNullOrEmpty(mname) ? Path.GetFileName(file) : mname;
+                foreach (var embed in Resources)
+                {
+                    var file = embed.ItemSpec;
+                    var mname = embed.GetMetadata("Name");
+                    var name = string.IsNullOrEmpty(mname) ? Path.GetFileName(file) : mname;
 
-                LogMessage("Embedding resource: {0} as {1}", file, name);
-                resources.Add(new EmbeddedResource(name, ManifestResourceAttributes.Public, File.OpenRead(file)));
+                    var modname = Path.ChangeExtension(file, "netmodule");
+
+                    using var modass = AssemblyDefinition.ReadAssembly(file, new ReaderParameters { ReadSymbols = false, ReadingMode = ReadingMode.Immediate });
+                    modass.MainModule.Name = name;
+                    modass.MainModule.Kind = ModuleKind.NetModule;
+                    modass.MainModule.Write(modname, new WriterParameters { WriteSymbols = false });
+
+                    LogMessage("Adding module: {0} as {1}", file, name);
+
+                    var newmod = ModuleDefinition.ReadModule(modname, new ReaderParameters { ReadSymbols = false, ReadingMode = ReadingMode.Immediate });
+                    ass.Modules.Add(newmod);
+                    //ass.MainModule.ModuleReferences.Add(newmod); // not really a ref is it....
+                }
+            }
+            else
+            {
+                var resources = ass.MainModule.Resources;
+
+                foreach (var embed in Resources)
+                {
+                    var file = embed.ItemSpec;
+                    var mname = embed.GetMetadata("Name");
+                    var name = string.IsNullOrEmpty(mname) ? Path.GetFileName(file) : mname;
+
+                    LogMessage("Embedding resource: {0} as {1}", file, name);
+                    using var fs = File.OpenRead(file);
+                    var ms = new MemoryStream();
+                    fs.CopyTo(ms);
+                    ms.Position = 0;
+                    resources.Add(new EmbeddedResource(name, ManifestResourceAttributes.Public, ms));
+                }
             }
 
             LogMessage("Saving assembly: {0}", output);
